6fc7982f66fe489065abae6d8263e8fa0121bdf4
adonai <adonai@in8.com.br>
2022-11-22 16:18:33 -0300

    TEST: component TabButton init

---
+++ src/components/Header/children_component/TabButton.test.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import TabButton from './TabButton';
+
+const props = {
+    title: 'Something',
+    href: '/some/path'
+}
+
+test('renders tab button with title through props', () => {
+    render(<TabButton {...props}/>);
+    const tabButton = screen.getByText('Something');
+    expect(tabButton).toBeInTheDocument();
+});
+  
+test('tab button can be clicked', () => {
+    render(<TabButton />);
+    const tabButton = screen.getByText('Something');
+    fireEvent.click(tabButton);
+});

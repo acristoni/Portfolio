73d3b2f293f49fd8db9bcdf3ca723ebf42b9b9ef
adonai <adonai@in8.com.br>
2022-11-22 14:28:27 -0300

    TEST: About refact

--- src/components/About/About.test.tsx
+++ src/components/About/About.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import About from './About';
 
 test('renders about me main content (Title - component)', () => {

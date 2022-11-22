891484934f6109e119c4fee3f9acb2e6c16fb88c
adonai <adonai@in8.com.br>
2022-11-22 16:39:58 -0300

    TEST: refact App

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,32 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const header = screen.getByTestId('header');
+  expect(header).toBeInTheDocument();
+});
+
+test('renders app, with About component', () => {
+  render(<App />);
+  const about = screen.getByTestId('about');
+  expect(about).toBeInTheDocument();
+});
+
+test('renders app, with Skills component', () => {
+  render(<App />);
+  const skills = screen.getByTestId('skills');
+  expect(skills).toBeInTheDocument();
+});
+
+test('renders app, with Works component', () => {
+  render(<App />);
+  const works = screen.getByTestId('works');
+  expect(works).toBeInTheDocument();
+});
+
+test('renders app footer', () => {
+  render(<App />);
+  const footer = screen.getByTestId('footer');
+  expect(footer).toBeInTheDocument();
 });

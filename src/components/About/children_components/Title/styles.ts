11896de68ce6b0e9778135c57692395bbf425d9e
adonai <adonai@in8.com.br>
2022-11-23 10:26:53 -0300

    REFACT: Title component styles

--- src/components/About/children_components/Title/styles.ts
+++ src/components/About/children_components/Title/styles.ts
@@ -4,7 +4,7 @@ export const Container = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    width: 85%;
+    width: 50%;
 `;
 
 export const Name = styled.h1`

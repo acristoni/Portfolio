a3450d87ff656e34ed29a7c8d7b49b5572e91de3
adonai <adonai@in8.com.br>
2022-11-24 16:14:48 -0300

    FEAT: header invisible scroll down

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import subMenusHeader from '../../data/subMenusHeader.data'
 import ButtonTheme from './children_component/ButtonTheme/ButtonTheme';
 import Name from './children_component/Name/Name';
@@ -6,8 +6,34 @@ import TabButton from './children_component/TabButton/TabButton';
 import { SHeader, Container, LeftContent, RightContent } from './styles';
 
 function Header() {
+  const [offset, setOffset] = useState(0);
+  const [prevOffSet, setPrevOffset] = useState(0);
+  const [visible, setVisible] = useState(true)
+
+  useEffect(() => {
+      const onScroll = () => setOffset(window.pageYOffset);
+      // clean up code
+      window.removeEventListener('scroll', onScroll);
+      window.addEventListener('scroll', onScroll, { passive: true });
+      return () => window.removeEventListener('scroll', onScroll);
+  }, []);
+
+  useEffect(()=>{
+    setTimeout(()=>{
+        setPrevOffset(offset)
+      }, 1000
+    )
+  },[offset])
+  
+  useEffect(()=>{
+    if ( offset <= prevOffSet ) {
+      return setVisible(true)
+    }
+    return setVisible(false)
+  },[prevOffSet])
+
   return (
-    <SHeader data-testid="header">
+    <SHeader data-testid="header" style={{'display': `${visible ? 'flex' : 'none '}`}}>
       <Container>
         <LeftContent>
           <Name/>
--- src/components/Header/children_component/Name/styles.ts
+++ src/components/Header/children_component/Name/styles.ts
@@ -8,7 +8,7 @@ export const Title = styled.div`
 export const TextTitle = styled.h1`
   margin: 0;
   font-weight: 400;
-  font-size: 40px;
+  font-size: 36px;
   font-family: ${props => props.theme.fonts.name};
 `
 
